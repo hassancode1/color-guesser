297e8a89ef567fa9c827d5837b2e378d4fbd705d
unknown <hassan.jaeh@flexisaf.com>
2023-03-13 12:50:46 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+ color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+} 
+
 function App() {
+  const [color, setColor] = useState("")
+  const [answers, setAnswers] = useState([])
+const [isCorrect , setIsCorrect]= useState(false)
+const [isWrong , setIsWrong]= useState(false)
+function generateColor(){
+  const actualColor= getRandomColor()
+  setColor(actualColor)
+  setAnswers([actualColor, getRandomColor(),getRandomColor()].sort(() => Math.random() - 0.5))
+}
+
+  useEffect(() =>{
+  generateColor()
+  }, [])
+  
+  function handleAnswers (answer){
+if(answer === color){
+  generateColor()
+  setIsCorrect(true)
+setTimeout(() => {
+  setIsCorrect(false)
+}, 1000);
+
+
+}
+else{
+setIsWrong(true)
+setTimeout(() => {
+  setIsWrong(false)
+}, 1000);
+}
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div className='color' style={{backgroundColor:`${color}` }}></div>
+ <div className='answers'>
+{answers?.map((answer,i)=>
+  <button key={i} onClick={() => handleAnswers(answer)}>{answer}</button>
+  )}
+   </div>
+  <div className={ `${isCorrect ? "correct" :""}`}>{isCorrect ? "Yay is correct" : ''}</div>
+<div className={ `${isWrong ? "wrong" :""}`}>{isWrong ? "nope wrong" : ''}</div>
     </div>
   );
 }
